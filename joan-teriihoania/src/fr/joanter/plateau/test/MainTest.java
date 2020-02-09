8d7d0fd7d17b68d0bc2feb872777dabc6211f370
Joprocorp <joprocorp@gmail.com>
2020-02-09 19:59:49 +0100

    Adding classes

---
+++ joan-teriihoania/src/fr/joanter/plateau/test/MainTest.java
@@ -0,0 +1,14 @@
+package fr.joanter.plateau.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void first_test(){
+        System.out.println("test");
+    }
+
+}
