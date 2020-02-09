72b935be7e13bdd8abfcdd40b93cdc23017f4df3
Joprocorp <joprocorp@gmail.com>
2020-02-09 20:31:02 +0100

    Completing constructor Mouvement

--- joan-teriihoania/src/fr/joanter/plateau/main/Mouvement.java
+++ joan-teriihoania/src/fr/joanter/plateau/main/Mouvement.java
@@ -13,21 +13,24 @@ public class Mouvement {
         this.moves = getRandomMoves(individu.getPas());
     }
 
+    public Mouvement(String[] moves, Individu individu) {
+        this.moves = moves;
+        this.individu = individu;
+    }
+
+    /*
+    * TODO: Check for valid moves
+    * */
     private String[] getRandomMoves(int n){
         String[] moves = new String[n];
         for (int i = 0;i < n;i++){
-            moves[i] = getRandomMove();
+            moves[i] = getRandomMove(moves);
         }
         return moves;
     }
 
-    private String getRandomMove(){
-        int ran = new Random().nextInt(move.length);
-        return move[ran];
-    }
-
-    public Mouvement(String[] moves, Individu individu) {
-        this.moves = moves;
-        this.individu = individu;
+    private String getRandomMove(String[] validMove){
+        int ran = new Random().nextInt(validMove.length);
+        return validMove[ran];
     }
 }
