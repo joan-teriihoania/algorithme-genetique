616df4a822f55f3f5ef7b1e81540af1efb91cca8
Joprocorp <joprocorp@gmail.com>
2020-02-10 10:52:21 +0100

    If piece is touched, it is not counted anymore by the evaluation

--- joan-teriihoania/src/fr/joanter/plateau/main/Individu.java
+++ joan-teriihoania/src/fr/joanter/plateau/main/Individu.java
@@ -21,29 +21,37 @@ public class Individu {
         return plateau;
     }
 
-    /*
-    * WARN: Capital++ if individu touch the same piece multiple times
-    * TODO: If piece is touched, it is not counted anymore by the evaluation
-    * */
     public int evaluate(){
+        // Initialise visited cases map
+        Boolean[][] visited_coor = new Boolean[this.plateau.getSize()][this.plateau.getSize()];
+        for (int i = 0;i < visited_coor.length;i++){
+            for (int j = 0;j < visited_coor.length;j++){
+                visited_coor[i][j] = false;
+            }
+        }
+
         int x = this.plateau.getX();
         int y = this.plateau.getY();
         int capital = 0;
-        if (this.plateau.caseHasPiece(x, y)){
+        // If case has piece and is not visited, add capital
+        // If case has no piece or is visited, remove capital
+        if (this.plateau.caseHasPiece(x, y) && !visited_coor[x][y]){
             capital++;
         } else {
             capital--;
         }
+        visited_coor[x][y] = true;
         for (String move: this.moves.getMoves()) {
             if (move.equals("H")){y++;}
             if (move.equals("B")){y--;}
             if (move.equals("G")){x--;}
             if (move.equals("D")){x++;}
-            if (this.plateau.caseHasPiece(x, y)){
+            if (this.plateau.caseHasPiece(x, y) && !visited_coor[x][y]){
                 capital++;
             } else {
                 capital--;
             }
+            visited_coor[x][y] = true;
         }
         return capital;
     }
