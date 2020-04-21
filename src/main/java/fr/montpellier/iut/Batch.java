c7d2c1687e80da99635e98f6d9eb8f4054b326a6
Joprocorp <joprocorp@gmail.com>
2020-04-21 15:36:45 +0200

    Correction orthographe

--- src/main/java/fr/montpellier/iut/Batch.java
+++ src/main/java/fr/montpellier/iut/Batch.java
@@ -13,7 +13,7 @@ public class Batch {
         Plateau copy = new Plateau(plateau);
         for (int i = 0 ; i < iterations ; i++){
             String save_filename = copy.run(cycles);
-            System.out.println("Population de l'itération n°"+(i+1)+" enregistrée dans 'result/"+save_filename+"'\n");
+            System.out.println("Population de l'itération n°"+(i+1)+" enregistrée dans 'result/"+save_filename+"'");
         }
         System.out.println("Exécution terminée.");
     }
